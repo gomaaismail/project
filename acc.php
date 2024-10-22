4e32ad94e99ba84ce0c52f74ac97f67a3cfd79cb
gomaa ismail <gomaa.ismail2025@gmail.com>
2024-10-22 11:23:56 +0300

    Add files via upload

---
+++ acc.php
@@ -0,0 +1,44 @@
+<html lang="en">
+<head>
+<title>اموال لأنظمة الحسابات العامة</title>
+
+<link rel="stylesheet" href="go.css">
+
+
+</head>
+<body>
+<nav>
+
+<ul>
+<li><a href="#">الاكواد</a>
+<ul>
+<li><a href="#">الشركات</a></li>
+<li><a href="#">الفروع</a></li>
+<li><a href="#">المدن والمناطق</a></li>
+<li><a href="#">المجموعات المحاسبية</a></li>
+<li><a href="#">العملات</a></li>
+<li><a href="#">أسعار تحويل العملات</a></li>
+<li><a href="#">البنوك</a></li>
+<li><a href="#">الخزن</a></li>
+<li><a href="#">الموظفين</a></li>
+<li><a href="#">مراكز التكلفة</a></li>
+<li><a href="#">العملاء</a></li>
+<li><a href="#">الموردين</a></li>
+</ul>
+</li>
+<li><a href="#">الخزينة</a></li>
+<li><a href="#">الحسابات العامة</a></li>
+<li><a href="#">إدارة النظام</a></li>
+<li><a href="#">التحكم</a></li>
+</ul>
+</nav>
+
+
+
+
+
+
+
+<script src="jave.js"></script>
+</body>
+</html>
